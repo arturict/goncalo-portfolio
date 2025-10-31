83e98f4cc5bdb68d83486c910b692708f65c6c3d
Artur Ferreira <artur.fer.cruz@gmail.com>
2025-10-31 10:18:13 +0100

    fix: add PostHog CSP headers for CORS

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://eu-assets.i.posthog.com",
+              "connect-src 'self' https://eu.i.posthog.com https://eu-assets.i.posthog.com",
+              "img-src 'self' data: https:",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' data:",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
